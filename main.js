c38c7214a83ef72b43a3368b09d076f8f982e30c
mikewiner <winer.mike@gmail.com>
2020-09-17 12:17:17 -0400

    proof of work

--- main.js
+++ main.js
@@ -7,15 +7,24 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   calculateHash () {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+  mineBlock (difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined:" + this.hash)
   }
 }
 
 class Blockchain {
   constructor(){
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 6;
   }
 
   createGenesisBlock() {
@@ -27,7 +36,8 @@ class Blockchain {
 
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty)
+    // newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
   isChainValid(){
@@ -47,14 +57,18 @@ class Blockchain {
 }
 
 let mikeCoin = new Blockchain();
+
+console.log('mining coin 1...')
 mikeCoin.addBlock(new Block(1, "01/02/2020", {amount:4} ))
+
+console.log('mining coin 2...')
 mikeCoin.addBlock(new Block(2, "01/02/2020", {amount:10} ))
 
-console.log(JSON.stringify(mikeCoin, null, 4));
-console.log('is valid?:',mikeCoin.isChainValid());
+// console.log(JSON.stringify(mikeCoin, null, 4));
+// console.log('is valid?:',mikeCoin.isChainValid());
 
-mikeCoin.chain[1].data = {amount: 10000000};
-mikeCoin.chain[1].hash = mikeCoin.chain[1].calculateHash();
+// mikeCoin.chain[1].data = {amount: 10000000};
+// mikeCoin.chain[1].hash = mikeCoin.chain[1].calculateHash();
 
-console.log('is valid?:',mikeCoin.isChainValid());
+// console.log('is valid?:',mikeCoin.isChainValid());
 
